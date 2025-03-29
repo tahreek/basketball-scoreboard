eabe4c704a68c4571641476f2be25ca22d1c17ac
tahreek <leetahreek@gmail.com>
2025-03-29 07:02:22 -0400

    commit initial files

---
+++ index.js
@@ -0,0 +1,38 @@
+let homeEl = document.getElementById("home-el")
+let awayEl = document.getElementById("away-el")
+let countHome = 0
+let countAway = 0
+
+function increment1home() {
+    countHome += 1
+    homeEl.textContent = countHome
+}
+
+function increment2home() {
+    countHome += 2
+    homeEl.textContent = countHome
+}
+
+function increment3home() {
+    countHome += 3
+    homeEl.textContent = countHome
+}
+
+function increment1away() {
+    countAway += 1
+    awayEl.textContent = countAway
+}
+
+function newGame() {
+    
+}
+
+function increment2away() {
+    countAway += 2
+    awayEl.textContent = countAway
+}
+
+function increment3away() {
+    countAway += 3
+    awayEl.textContent = countAway
+}
